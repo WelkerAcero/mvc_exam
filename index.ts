4e4aea7c4bb51afd35f50c1e03f213e24496cf33
Jean <jeancagomez94@gmail.com>
2022-09-06 15:03:34 -0500

    Merge con jeancito

--- index.ts
+++ index.ts
@@ -1,42 +1,14 @@
-import express from "express";
-import { ProductModel } from "./App/Models/ProductModel";
+import App from './app';
 
-let obj = new ProductModel();
-const app = express();
+class Server extends App {
+    private _PORT:number = this.app.get('PORT')
 
-app.use(express.json())
+    listen(){
+        this.app.listen(this._PORT, ()=> {
+            console.log('listening on port ' + this._PORT)
+        })
+    }
+}
 
-app.get('/', async (req, res) => {
-    /* console.log('Page index'); */
-    let data = await obj.get_data("QUERY");
-    res.send(data);
-})
-
-app.get('/products', (req, res) => {
-
-    //Query a database
-    //Process data
-    //Validate data
-
-    res.send('Product list')
-})
-
-app.post('/products', (req, res) => {
-    res.send('Creating products')
-})
-
-app.put('/products', (req, res) => {
-    res.send('Updating products')
-})
-
-app.delete('/products', (req, res) => {
-    res.send('Deleted product')
-})
-
-app.patch('/products', (req, res) => {
-    res.send('Updating a part of the product')
-})
-
-app.listen(3000, () => {
-    console.log("Initialization server");
-})
+const ser = new Server();
+ser.listen()
