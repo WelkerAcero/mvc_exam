7f3625d519a519278186c36f6cfb666fa37eeaaf
simonhl14 <simonh419@gmail.com>
2022-09-06 16:50:17 -0500

    routes-funcionales v2

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import App from './app';
 import Connection from './App/DB/connection';
 import userRoutes from './App/routes/user';
+import userBanRoutes from './App/routes/userBan';
+import commentRoutes from './App/routes/comments';
+import productRoutes from './App/routes/product';
+
 
 class Server extends App {
     private _PORT:number = this.app.get('PORT')
 
     routes() {
         this.app.use(userRoutes);
+        this.app.use(userBanRoutes);
+        this.app.use(commentRoutes);
+        this.app.use(productRoutes);
     }
 
     listen(){
