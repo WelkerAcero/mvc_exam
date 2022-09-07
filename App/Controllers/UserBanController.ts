83ae36d97116bc52966fc3af2ca59b98d664acf9
Mishell  Merchán <mishell2172329@correo.uis.edu.co>
2022-09-06 23:32:22 -0500

    UserBanController

--- App/Controllers/UserBanController.ts
+++ App/Controllers/UserBanController.ts
@@ -3,10 +3,19 @@ import { ModelQuery } from "../Models/ModelQuery";
 
 let obj = new ModelQuery();
 
-export const getUsersBan = async (req: Request, res: Response) => {
-    
-    const body = req.body.idArticle;
-    let bringData = obj.get_data(`SELECT * FROM mvc_exam.usersBlock WHERE iduserblock = ${body} ;`);
+export const getUsersBan =  async (req: Request, res: Response) => {
+
+    let bringData = obj.get_data("select * from mvc_exam.usersBlock;");
+    const data = await bringData;
+    console.log(data);
+    return res.json(data);
+}
+
+export const getUserBan = async (req: Request, res: Response) => {
+    //Usuarios bloqueados de un artículo
+    const  id  = req.params.id;
+
+    let bringData = obj.get_data(`SELECT * FROM mvc_exam.usersBlock WHERE idArticle = ${id};`);
     const data = await bringData;
     console.log(data);
     return res.json(data);
@@ -19,7 +28,7 @@ export const postUserBan = async (req: Request, res: Response) => {
    
     const bod = req.body;
     let bringData = obj.put_data(`INSERT INTO mvc_exam.usersBlock(idUserBlock,idUser,idArticle) VALUES (UUID(),UUID(), UUID() );`);
-    return res.json(`Se creo correctamente`);
+    return res.json(`Se creó correctamente`);
 
 } 
 
--- App/routes/userBan.ts
+++ App/routes/userBan.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { getUsersBan, postUserBan, putUserBan, deleteUserBan  } from '../Controllers/UserBanController';
+import { getUsersBan, getUserBan, postUserBan, putUserBan, deleteUserBan  } from '../Controllers/UserBanController';
 import body from 'body-parser'
 
 const router = Router();
 const parse = body.json()
 
-router.get('/userban/:idArticle',parse, getUsersBan);
+router.get('/userban/', parse, getUsersBan);
+router.get('/userban/:idArticle',parse, getUserBan);
 router.post('/userban',parse, postUserBan);
 router.put('/userban/:idArticle/:idUser',parse, putUserBan);
 router.delete('/userban/:idArticle/:idUser',parse, deleteUserBan);
