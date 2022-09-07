270ad67a9711e3b346265bcc7acec7f1b2233fc6
Jean <jeancagomez94@gmail.com>
2022-09-06 22:57:16 -0500

    Controller de userblock

--- App/Controllers/UserBanController.ts
+++ App/Controllers/UserBanController.ts
@@ -5,7 +5,8 @@ let obj = new ModelQuery();
 
 export const getUsersBan = async (req: Request, res: Response) => {
     
-    let bringData = obj.get_data("select * from articles;");
+    const body = req.body.idArticle;
+    let bringData = obj.get_data(`SELECT * FROM mvc_exam.usersBlock WHERE iduserblock = ${body} ;`);
     const data = await bringData;
     console.log(data);
     return res.json(data);
@@ -14,11 +15,11 @@ export const getUsersBan = async (req: Request, res: Response) => {
 
 export const postUserBan = async (req: Request, res: Response) => {
     
+    //Falta validaciones
    
     const bod = req.body;
-    
-    let bringData = obj.put_data(`INSERT INTO mvc_exam.articles(idUser,idArticle,name,description) VALUES (UUID(),UUID(), '${ bod.name }', '${ bod.description }');`);
-    return res.json(`Se creo correctamente ${bod.name}`);
+    let bringData = obj.put_data(`INSERT INTO mvc_exam.usersBlock(idUserBlock,idUser,idArticle) VALUES (UUID(),UUID(), UUID() );`);
+    return res.json(`Se creo correctamente`);
 
 } 
 
--- App/routes/userBan.ts
+++ App/routes/userBan.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { getUsersBan, postUserBan, putUserBan, deleteUserBan  } from '../Controllers/UserBanController';
+import body from 'body-parser'
 
 const router = Router();
+const parse = body.json()
 
-router.get('/userban/:idArticle', getUsersBan);
-router.post('/userban/:idArticle', postUserBan);
-router.put('/userban/:idArticle/:idUser', putUserBan);
-router.delete('/userban/:idArticle/:idUser', deleteUserBan);
+router.get('/userban/:idArticle',parse, getUsersBan);
+router.post('/userban',parse, postUserBan);
+router.put('/userban/:idArticle/:idUser',parse, putUserBan);
+router.delete('/userban/:idArticle/:idUser',parse, deleteUserBan);
 
 export default router;
